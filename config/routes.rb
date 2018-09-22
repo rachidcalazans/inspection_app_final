0c1689d029ef2e6af1463f0488fa0fe330a37000
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 00:20:11 -0300

    master Update routes
    - add route for photos_controller for create action
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      namespace :inspections do
+        post ':inspection_id/photos', to: 'photos#create'
+      end
+    end
+  end
 end
