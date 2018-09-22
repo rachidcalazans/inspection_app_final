d8c55327101668c17fac968575baf7c761e7be54
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 00:20:41 -0300

    master Update photos_controller_spec
    - add create endpoint describe
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- spec/controllers/api/v1/inspections/photos_controller_spec.rb
+++ spec/controllers/api/v1/inspections/photos_controller_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 module Api
   module V1
     module Inspections
-      describe PhotosController do
+      describe PhotosController, type: :controller do
 
-        it 'It is working' do
-          expect(true).to be true
+        describe 'POST /api/v1/inspections/:inspection_id/photos' do
+
+          before do
+            post :create, params: { inspection_id: 1 }
+          end
+
+          it 'Should create an inspection photo' do
+            expect(response.status).to be == 201
+          end
         end
       end
     end
