f7405ea9f52e285d05b727a7bb5d05a147133965
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 00:53:17 -0300

    master Update implementation for photos controller
    - add the required params
    - update tests setup
    - update create action implementation
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- app/controllers/api/v1/inspections/photos_controller.rb
+++ app/controllers/api/v1/inspections/photos_controller.rb
@@ -4,7 +4,26 @@ module Api
       class PhotosController < ActionController::API
 
         def create
-          render json: {}, status: 201
+          Struct.new('InspectionPhotoPresenter', :id, :inspection_id, :photo_url, :caption, keyword_init: true)
+          request_params = photo_params
+            .merge(id: 11, photo_url: url_from_base_64(photo_params[:photo_base_64]))
+            .except(:photo_base_64)
+            .to_h
+
+          presenter = Struct::InspectionPhotoPresenter.new(request_params)
+
+          render json: presenter, status: 201
+        end
+
+        private
+
+        def url_from_base_64(base_64)
+          "http://chid.com/inspection/photo_#{base_64}"
+        end
+
+        def photo_params
+          permitted_keys = %i[inspection_id photo_base_64 caption]
+          params.permit(permitted_keys)
         end
       end
     end
--- spec/controllers/api/v1/inspections/photos_controller_spec.rb
+++ spec/controllers/api/v1/inspections/photos_controller_spec.rb
@@ -6,13 +6,25 @@ module Api
       describe PhotosController, type: :controller do
 
         describe 'POST /api/v1/inspections/:inspection_id/photos' do
+          let(:json) { JSON.parse response.body }
 
           before do
-            post :create, params: { inspection_id: 1 }
+            post :create, params: { inspection_id: 1, photo_base_64: 'some_hash_64' }
           end
 
-          it 'Should create an inspection photo' do
-            expect(response.status).to be == 201
+          context 'When given required params' do
+
+            it 'Should create an inspection photo' do
+              expect(response.status).to be == 201
+
+              expected_response = {
+                'id'            => 11,
+                'inspection_id' => '1',
+                'photo_url'     => 'http://chid.com/inspection/photo_some_hash_64',
+                'caption'       => nil,
+              }
+              expect(json).to eq expected_response
+            end
           end
         end
       end
