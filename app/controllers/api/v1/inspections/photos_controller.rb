c6b8dc86ee623347c304e4e84db790a07a0827fa
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 00:19:37 -0300

    master Update photos_controller
    - add create action
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- app/controllers/api/v1/inspections/photos_controller.rb
+++ app/controllers/api/v1/inspections/photos_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     module Inspections
       class PhotosController < ActionController::API
+
+        def create
+          render json: {}, status: 201
+        end
       end
     end
   end
