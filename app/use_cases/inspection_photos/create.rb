66d0ec8923e43cb43e1ba95d834f4fefb37da365
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 02:01:43 -0300

    master Update InspectionPhoto Create use case
    - update to use the Presenter class
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- app/use_cases/inspection_photos/create.rb
+++ app/use_cases/inspection_photos/create.rb
@@ -9,24 +9,21 @@ module UseCases
       end
 
       def call
-        Struct.new('InspectionPhotoPresenter', :id, :inspection_id, :photo_url, :caption, keyword_init: true)
-        request_params = request
-          .to_h
-          .merge(photo_url: url_from_base_64(request.photo_base_64))
-          .except(:photo_base_64)
+        inspection_photo = ::InspectionPhoto.create(inspection_photo_params)
 
-        inspection_photo = ::InspectionPhoto.create(request_params)
-
-        presenter_params = inspection_photo
-          .attributes
-          .except('created_at', 'updated_at')
-
-        Struct::InspectionPhotoPresenter.new(presenter_params)
+        ::Presenters::InspectionPhoto.new(inspection_photo)
       end
 
       private
 
       attr_reader :request
+
+      def inspection_photo_params
+        request
+          .to_h
+          .merge(photo_url: url_from_base_64(request.photo_base_64))
+          .except(:photo_base_64)
+      end
 
       def url_from_base_64(base_64)
         "http://chid.com/inspection/photo_#{base_64}"
