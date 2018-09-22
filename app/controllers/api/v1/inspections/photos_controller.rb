67d60d6ed299f02055964e1af8d9428a4ce51eec
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 01:37:56 -0300

    master Update PhotosController
    - use the new UseCase to create InspectionPhoto
    - remove all logic from controller
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- app/controllers/api/v1/inspections/photos_controller.rb
+++ app/controllers/api/v1/inspections/photos_controller.rb
@@ -4,27 +4,15 @@ module Api
       class PhotosController < ActionController::API
 
         def create
-          Struct.new('InspectionPhotoPresenter', :id, :inspection_id, :photo_url, :caption, keyword_init: true)
-          request_params = photo_params
-            .merge(id: 11, photo_url: url_from_base_64(photo_params[:photo_base_64]))
-            .except(:photo_base_64)
-            .to_h
+          photo_request = ::UseCases::InspectionPhotos::Create::Request.new(photo_params.to_h)
+          use_case      = ::UseCases::InspectionPhotos::Create.new(photo_request)
 
-          inspection_photo = ::InspectionPhoto.create(request_params)
-
-          presenter_params = inspection_photo
-            .attributes
-            .except('created_at', 'updated_at')
-          presenter = Struct::InspectionPhotoPresenter.new(presenter_params)
+          presenter = use_case.call
 
           render json: presenter, status: 201
         end
 
         private
-
-        def url_from_base_64(base_64)
-          "http://chid.com/inspection/photo_#{base_64}"
-        end
 
         def photo_params
           permitted_keys = %i[inspection_id photo_base_64 caption]
