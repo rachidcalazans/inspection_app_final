a1782c3128ac2956c83b311b30dd65792f67ba89
Rachid Calazans <rachidcalazans@gmail.com>
2018-09-22 01:16:59 -0300

    master Update photos_controller spec
    - update setup to create a real Inspection
    - update the expected_response for inspection_id to be integer
    - replace the compare method eq for match
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- spec/controllers/api/v1/inspections/photos_controller_spec.rb
+++ spec/controllers/api/v1/inspections/photos_controller_spec.rb
@@ -8,8 +8,10 @@ module Api
         describe 'POST /api/v1/inspections/:inspection_id/photos' do
           let(:json) { JSON.parse response.body }
 
+          let(:inspection) { ::Inspection.create(description: 'Inspection 001') }
+
           before do
-            post :create, params: { inspection_id: 1, photo_base_64: 'some_hash_64' }
+            post :create, params: { inspection_id: inspection.id, photo_base_64: 'some_hash_64' }
           end
 
           context 'When given required params' do
@@ -19,11 +21,12 @@ module Api
 
               expected_response = {
                 'id'            => 11,
-                'inspection_id' => '1',
+                'inspection_id' => 1,
                 'photo_url'     => 'http://chid.com/inspection/photo_some_hash_64',
                 'caption'       => nil,
               }
-              expect(json).to eq expected_response
+
+              expect(json).to match expected_response
             end
           end
         end
